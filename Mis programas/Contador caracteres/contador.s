35c6428967ec42907d9667cb1be37da0d72f5909
Miguelsm255 <miguel.desandemoreno@gmail.com>
2024-03-05 22:55:36 +0100

    Comentarios en Contador

--- Mis programas/Contador caracteres/contador.s
+++ Mis programas/Contador caracteres/contador.s
@@ -1,40 +1,48 @@
+###################################
+#    CONTADOR DE CARACTERES       #
+#                                 #
+# Cuenta el número de caracteres  #
+#  introducidos por terminal al   #
+#      ejecutar el programa.      #
+# (inestable, sin comprobaciones) #
+###################################
 
 .data
-    len = 21
-    buf: .zero len
+    len  =   10
+    buf:    .zero len
     argc:   .byte 0
     LF:     .byte 0x0A
 
 .text
     .global _start
     _start:
-        movl $3,            %eax            # LEER
-        movl $0,            %ebx            # DEL TECLADO
-        movl $buf,          %ecx            # ESCRIBIENDOLO EN BUF
-        movl $len,          %edx            # CON MÁXIMO ESTA LONGITUD
+        movl $3,        %eax            # LEER
+        movl $0,        %ebx            # DEL TECLADO
+        movl $buf,      %ecx            # ESCRIBIENDOLO EN BUF
+        movl $len,      %edx            # CON MÁXIMO ESTA LONGITUD
         int  $0x80
 
-        movl %eax, %esi                     # GUARDAR NUMERO DE CARACTERES INTRODUCIDOS EN ESI
+        movl %eax,      %esi            # GUARDAR NUMERO DE CARACTERES INTRODUCIDOS EN ESI
 
-        movl %esi,      %ecx                # GUARDO EL NUMERO DE ARGUMENTOS EN ECX
-        decl %ecx                           
-        orb  $0x30,     %cl                 # CONVIERTO EL NUMERO DE ARGUMENTOS A ASCII
-        movb %cl,       argc
-
-
-        movl $4, %eax
-        movl $1, %ebx
-        movl $argc, %ecx
-        movl $1, %edx
-        int $0x80
-
-        movl $4, %eax
-        movl $1, %ebx
-        movl $LF, %ecx
-        movl $1, %edx
-        int $0x80
+        movl %esi,      %ecx            # GUARDAR NUMERO DE CARACTERES INTRODUCIDOS EN ECX
+        decl %ecx                       # DECREMENTO EL NÚMERO PARA ELIMINAR EL LF
+        orb  $0x30,     %cl             # CONVIERTO EL NUMERO DE ARGUMENTOS A ASCII
+        movb %cl,       argc            # GUARDO EL NUMERO DE ARGUMENTOS EN ARGC
 
 
-        movl $1,     %eax                   # DEVOLUCIÓN DEL CONTROL
-        movl $0,     %ebx                   # AL SISTEMA OPERATIVO
+        movl $4,        %eax            # ESCRIBIR
+        movl $1,        %ebx            # EN PANTALLA
+        movl $argc,     %ecx            # EL NUMERO DE CARACTERES INTRODUCIDOS
+        movl $1,        %edx            # CON MÁXIMO ESTA LONGITUD
+        int  $0x80
+
+        movl $4,        %eax            # ESCRIBIR
+        movl $1,        %ebx            # EN PANTALLA
+        movl $LF,       %ecx            # EL SALTO DE LINEA
+        movl $1,        %edx            # CON LONGITUD 1
+        int  $0x80
+
+
+        movl $1,     %eax               # DEVOLUCIÓN DEL CONTROL
+        movl $0,     %ebx               # AL SISTEMA OPERATIVO
         int  $0x80
